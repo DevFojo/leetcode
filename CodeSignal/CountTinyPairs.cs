71b7c53577b8ae613bb0a53c6ce8303651454def
Femi Ojo <femiojo24@gmail.com>
2020-02-13 19:46:28 +0100

    Completed countTinyPairs problem

---
+++ CodeSignal/CountTinyPairs.cs
@@ -0,0 +1,49 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CodeSignal
+{
+    public class CountTinyPairs
+    {
+        int countTinyPairs(int[] a, int[] b, int k)
+        {
+            var tinyCount = 0;
+            var n = a.Length;
+            for (var i = 0; i < n; i++)
+            {
+                var j = n - 1 - i;
+                var pow10 = getSize(b[j]) + 1;
+                var xy = a[i] * pow(10, pow10) + b[j];
+                if (xy < k)
+                    tinyCount++;
+            }
+
+            return tinyCount;
+        }
+
+        int getSize(int i)
+        {
+            var n = 0;
+            while (i > 10)
+            {
+                i /= 10;
+                n++;
+            }
+
+            return n;
+        }
+
+        int pow(int x, int y)
+        {
+            var ans = 1;
+            while (y > 0)
+            {
+                ans *= x;
+                y--;
+            }
+
+            return ans;
+        }
+    }
+}
