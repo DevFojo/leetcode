7420681a2bc5a0591af58cb62a279073bfc3fb60
Femi Ojo <femiojo24@gmail.com>
2020-02-13 20:00:39 +0100

    Completed firstDuplicate problem

---
+++ CodeSignal/FirstDuplicate.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace CodeSignal
+{
+    public class FirstDuplicate
+    {
+        int firstDuplicate(int[] a) {
+            var m = new Dictionary<int, int>();
+            for(var i = 0; i < a.Length; i++){
+                if(m.ContainsKey(a[i])){
+                    return a[i];
+                }
+                m[a[i]] = i;
+            }
+            return -1;
+        }
+
+    }
+}
