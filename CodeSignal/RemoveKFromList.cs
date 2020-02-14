af81cf09503cbd722ab005f3dea08d057286822f
Femi Ojo <femiojo24@gmail.com>
2020-02-14 10:35:33 +0100

    Completed removeKFromList problem

---
+++ CodeSignal/RemoveKFromList.cs
@@ -0,0 +1,34 @@
+using System.Text;
+using Xunit;
+
+namespace CodeSignal
+{
+    public class RemoveKFromList
+    {
+        ListNode<int> removeKFromList(ListNode<int> l, int k)
+        {
+            var head = l;
+            ListNode<int> previous = null;
+            var current = head;
+            while (current != null)
+            {
+                if (current.value == k)
+                {
+                    if (previous?.next == null)
+                    {
+                        head = current.next;
+                        current = head;
+                        continue;
+                    }
+
+                    previous.next = current.next;
+                }
+
+                previous = current;
+                current = current.next;
+            }
+
+            return head;
+        }
+    }
+}
