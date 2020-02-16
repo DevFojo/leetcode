ba5b271a40335ef2fb15a3b25d59b170ac3c2f80
Femi Ojo <femiojo24@gmail.com>
2020-02-16 09:18:50 +0100

    Completed Longest Palindrome problem : https://leetcode.com/problems/longest-palindromic-substring

---
+++ LeetCode/Solution5.cs
@@ -0,0 +1,49 @@
+namespace LeetCode
+{
+    public class Solution5
+    {
+        //TODO: can be further optimized
+        public string LongestPalindrome(string s)
+        {
+            var maxLength = 0;
+            var maxPalindrome = "";
+            var n = s.Length;
+
+            for (var i = 0; i < n; ++i)
+            {
+                for (var j = i; j < n; j++)
+                {
+                    var l = j - i + 1;
+                    if (l <= maxLength)
+                    {
+                        continue;
+                    }
+
+                    var sub = s.Substring(i, l);
+                    if (!IsPalindrome(sub))
+                    {
+                        continue;
+                    }
+
+                    maxLength = l;
+                    maxPalindrome = sub;
+                }
+            }
+
+            return maxPalindrome;
+        }
+
+        private bool IsPalindrome(string s)
+        {
+            for (var i = 0; i < (s.Length + 1) / 2; i++)
+            {
+                if (s[i] != s[s.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
