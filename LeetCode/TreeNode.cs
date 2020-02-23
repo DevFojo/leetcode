3fbafec4c35c2395470200558f6daba66edaf433
Femi Ojo <femiojo24@gmail.com>
2020-02-23 18:43:41 +0100

    Completed Average of Levels in Binary Tree problem: https://leetcode.com/problems/average-of-levels-in-binary-tree/

--- LeetCode/TreeNode.cs
+++ LeetCode/TreeNode.cs
@@ -1,3 +1,7 @@
+using Newtonsoft.Json;
+
+// ReSharper disable All
+
 namespace LeetCode
 {
     public class TreeNode
@@ -6,9 +10,38 @@ namespace LeetCode
         public TreeNode right;
         public int val;
 
+
         public TreeNode(int x)
         {
             val = x;
+        }
+
+        public TreeNode AddLeft(TreeNode l)
+        {
+            left = l;
+            return this;
+        }
+
+        public TreeNode AddLeft(int l)
+        {
+            return this.AddLeft(new TreeNode(l));
+        }
+
+        public TreeNode AddRight(TreeNode r)
+        {
+            right = r;
+            return this;
+        }
+
+        public TreeNode AddRight(int r)
+        {
+            return this.AddRight(new TreeNode(r));
+        }
+
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
         }
     }
 }
